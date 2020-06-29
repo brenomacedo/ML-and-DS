f7e12cfe81101f5e55f55308ae28b6beaa851ed6
brenosekiro <b88660501@gmail.com>
2020-06-29 20:08:31 -0300

    scaler

--- pre_processamento_census.py
+++ pre_processamento_census.py
@@ -10,15 +10,13 @@ import pandas as pd
 base = pd.read_csv('census.csv')
 
 previsores =  base.iloc[:, 0:14].values
-race = base.iloc[:, 8:9]
 classe = base.iloc[:, 14].values
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_previsores = LabelEncoder()
-labelencoder_race = LabelEncoder()
-# labels = labelencoder_previsores.fit_transform(previsores[:, 1])
+from sklearn.compose import ColumnTransformer
 
-race[:, 0] = labelencoder_race.fit_transform(race[:, 0])
+labelencoder_previsores = LabelEncoder()
+# labels = labelencoder_previsores.fit_transform(previsores[:, 1])
 previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
 previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:, 3])
 previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:, 5])
@@ -28,5 +26,14 @@ previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:, 8])
 previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:, 9])
 previsores[:, 13] = labelencoder_previsores.fit_transform(previsores[:, 13])
 
-onehotencoder = OneHotEncoder(categorical_features=[8])
+onehotencoder = ColumnTransformer(transformers=[("OneHot", OneHotEncoder(), [1,3,5,6,7,8,9,13])],remainder='passthrough')
 previsores = onehotencoder.fit_transform(previsores).toarray()
+
+labelencoder_classe = LabelEncoder()
+
+classe = labelencoder_classe.fit_transform(classe)
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
