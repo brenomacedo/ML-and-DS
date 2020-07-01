d9d2997d66439c6c8e3c2670a9fa897cca69e29a
brenosekiro <b88660501@gmail.com>
2020-07-01 11:02:34 -0300

    predictingvalues_confusionmatrix_accuracy

--- bayesian_learning/naive_bayes_credit_data.py
+++ bayesian_learning/naive_bayes_credit_data.py
@@ -1,8 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul  1 10:32:24 2020
+import pandas as pd
+base = pd.read_csv('credit_data.csv')
 
-@author: User
-"""
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
 
+from sklearn.impute import SimpleImputer
+import numpy as np
 
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(previsores[:, 0:3])
+
+previsores[:, 0:3] = imputer.transform(previsores[:, 0:3])
+
+from sklearn.preprocessing import StandardScaler
+
+scale = StandardScaler()
+previsores = scale.fit_transform(previsores)
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+from sklearn.naive_bayes import GaussianNB
+classificador = GaussianNB()
+classificador.fit(previsores_treinamento, classe_treinamento)
+
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
