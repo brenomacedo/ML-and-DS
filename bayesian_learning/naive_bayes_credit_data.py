ef4419fa2a07b7e7d51b4655740f8553a197cd25
brenosekiro <b88660501@gmail.com>
2020-07-01 10:38:17 -0300

    risco-credito-predicting-values

---
+++ bayesian_learning/naive_bayes_credit_data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 10:32:24 2020
+
+@author: User
+"""
+
+
--- bayesian_learning/naive_bayes_risco_credito.py
+++ bayesian_learning/naive_bayes_risco_credito.py
@@ -9,3 +9,22 @@ Created on Wed Jul  1 09:50:03 2020
 import pandas as pd
 base = pd.read_csv('risco_credito.csv')
 
+previsores = base.iloc[:, 0: 4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.preprocessing import LabelEncoder
+
+labelencoder_previsores = LabelEncoder()
+previsores[:, 0] = labelencoder_previsores.fit_transform(previsores[:, 0])
+previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
+previsores[:, 2] = labelencoder_previsores.fit_transform(previsores[:, 2])
+previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:, 3])
+
+from sklearn.naive_bayes import GaussianNB
+classificador = GaussianNB()
+classificador.fit(previsores, classe)
+
+resultado = classificador.predict([[0,0,1,2], [3,0,0,0]])
+print(classificador.classes_)
+print(classificador.class_count_)
+print(classificador.class_prior_)
