0489be52ab216dc9f29c6bd1a29fae65d16a3ecb
brenosekiro <b88660501@gmail.com>
2020-07-05 16:00:46 -0300

    regras-credit-data

---
+++ learning_rules/regras_credit_data.py
@@ -0,0 +1,17 @@
+import Orange
+base = Orange.data.Table('credit_data.csv')
+base.domain
+
+base_dividida = Orange.evaluation.testing.sample(base, n=0.25)
+
+base_treinamento = base_dividida[1]
+base_teste = base_dividida[0]
+
+cn2_learner = Orange.classification.rules.CN2Learner()
+classificador = cn2_learner(base_treinamento)
+
+for regras in classificador.rule_list:
+    print(regras)
+    
+resultado = Orange.evaluation.testing.TestOnTestData(base_treinamento, base_teste, [lambda testdata: classificador])
+print(Orange.evaluation.CA(resultado))
