b9f9692dbe9f156d245395d1de0af8ac2b55630f
brenosekiro <b88660501@gmail.com>
2020-07-05 15:42:49 -0300

    learning-rules-risco-credito

---
+++ learning_rules/regras_risco_credito.py
@@ -0,0 +1,13 @@
+import Orange
+base = Orange.data.Table('risco_credito.csv')
+
+cn2_learner = Orange.classification.rules.CN2Learner()
+classificador = cn2_learner(base)
+
+for regras in classificador.rule_list:
+    print(regras)
+    
+resultado = classificador([['boa', 'alta', 'nenhuma', 'acima_35'], ['ruim', 'alta', 'adequada', '0_15']])
+
+for i in resultado:
+    print(base.domain.class_var.values[i])
