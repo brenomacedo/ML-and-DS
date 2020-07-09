fc29d8631cce927cb8b5ddc8e8caa493c1ebcde0
brenosekiro <b88660501@gmail.com>
2020-07-08 21:12:23 -0300

    svm-credit-data

--- pre_processing/template_census.py
+++ pre_processing/template_census.py
@@ -8,6 +8,16 @@ classe = base.iloc[:, 14].values
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
+labelencoder_previsores = LabelEncoder()
+previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
+previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:, 3])
+previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:, 5])
+previsores[:, 6] = labelencoder_previsores.fit_transform(previsores[:, 6])
+previsores[:, 7] = labelencoder_previsores.fit_transform(previsores[:, 7])
+previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:, 8])
+previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:, 9])
+previsores[:, 13] = labelencoder_previsores.fit_transform(previsores[:, 13])
+
 column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [1, 3, 5, 6, 7, 8, 9, 13])],remainder='passthrough')
 previsores = column_tranformer.fit_transform(previsores).toarray()
 
