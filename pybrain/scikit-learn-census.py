a95c3405d8a0143f62d3e7bc727afed9c5bd89e3
brenosekiro <b88660501@gmail.com>
2020-08-29 20:09:36 -0300

    keras-credit-data

--- pybrain/scikit-learn-census.py
+++ pybrain/scikit-learn-census.py
@@ -1,8 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 29 19:00:28 2020
+import pandas as pd
 
-@author: User
-"""
+base = pd.read_csv('census.csv')
 
+previsores = base.iloc[:, 0:14].values
+classe = base.iloc[:, 14].values
+                
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
+labelencoder_previsores = LabelEncoder()
+previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
+previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:, 3])
+previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:, 5])
+previsores[:, 6] = labelencoder_previsores.fit_transform(previsores[:, 6])
+previsores[:, 7] = labelencoder_previsores.fit_transform(previsores[:, 7])
+previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:, 8])
+previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:, 9])
+previsores[:, 13] = labelencoder_previsores.fit_transform(previsores[:, 13])
+
+column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [1, 3, 5, 6, 7, 8, 9, 13])],remainder='passthrough')
+previsores = column_tranformer.fit_transform(previsores).toarray()
+
+labelencoder_classe = LabelEncoder()
+classe = labelencoder_classe.fit_transform(classe)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
+
+from sklearn.neural_network import MLPClassifier
+classificador = MLPClassifier(verbose = True, max_iter=1000, tol=0.000010)
+
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
